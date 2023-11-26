12790610b9ca6b4f031ce15fd98345023df468d6
Colibecas <Colibecas@HP255G8>
2023-11-25 21:17:37 -0600

    Agregar archivos de proyecto.

---
+++ Server/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using FilmCrud.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+namespace FilmCrud.Server.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+     
+        public virtual DbSet<Film> Films { get; set; }
+    }
+}
---
+++ Server/Migrations/20231126010154_Crear Tabla Films.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FilmCrud.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class CrearTablaFilms : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Films",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Duration = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdateAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Films", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Films");
+        }
+    }
+}
